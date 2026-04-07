c48d56e8a0c9bb8e09cfa07f8e15b7942fa57702
francisco <franciscoaraujo1076@gmail.com>
2026-04-07 14:53:51 -0300

    ADD models ✔

--- models.py
+++ models.py
@@ -1,3 +1,44 @@
 # models é o arquivo que fica as classes
+from sqlalchemy import create_engine, Column, Integer, String, ForeingKey
+from sqlalchemy.orm import declarative_base, sessionmaker, relationship
+
+Base = declarative_base()
+
+engine = create_engine("sqlite:///escola.db")
+ 
+Session = sessionmaker(bind=engine)
+
+#tabelas curso e Alunos (1:N)
 
 
+class  Curso(Base):
+    __tablename__ = "cursos"
+
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    nome = Column(String(100), nullable=False)
+    carga_horaria = Column(Integer, nullable=False)
+
+
+    alunos  = relationship("Aluno", back_populates="curso")
+
+
+    def __repr__(self):
+        return f"curso = id {self.id} - nome: {self.nome} - carga_horaria: {self.carga_horaria}"
+    
+
+
+
+class  Aluno(Base):
+    __tablename__ = "alunos"
+
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    nome = Column(String(100), nullable=False)
+    email = Column(String(100), unique=True)
+
+    cursos_id = Column(Integer, ForeingKey("cursos.id"))
+
+    curso = relationship("Curso", back_populates = "alunos")
+
+
+    def __repr__(self):
+        return f"alunos = id: {self.id} - nome: {self.nome} - Email: {self.email}"
